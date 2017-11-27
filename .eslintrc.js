63c32497006c5f51928793c5883f3991130c3939
Beau August <beauagst@gmail.com>
2017-11-27 15:47:22 +0000

    Added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
     "env": {
-        "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
+        "ecmaVersion": 8,
         "sourceType": "module"
     },
     "rules": {
