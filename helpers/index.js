267b56096490cf44507b1243907d583a88ed4a3e
Beau August <beauagst@gmail.com>
2017-11-28 14:52:16 +0000

    Started adding tests

--- helpers/index.js
+++ helpers/index.js
@@ -9,7 +9,13 @@ const scrollToBottom = async (page, iterations, iterator = 0) => {
     if (iterator !== iterations) return scrollToBottom(page, iterations, iterator);
 };
 
+const shuffleArray = (arr) => {
+    return arr.sort(() => Math.random() * 2 - 1);
+};
+
 const probability = (decimal) => {
+    if (typeof decimal !== 'number') throw new Error('paramater should be a number');
+    if (decimal > 1 || decimal < 0) throw new Error('paramater should be a value between 0 and 1');
     const number = Math.random();
     if (number > decimal) return false;
     return true;
@@ -23,16 +29,21 @@ const humanDelay = async (page) => {
 };
 
 const randomInArray = (arr) => {
+    if (!Array.isArray(arr)) throw new Error('arr paramater should be an array');
     return arr[Math.floor(Math.random()*arr.length)];
 };
 
 const trimArray = (arr, limit) => {
+    if (!Array.isArray(arr)) throw new Error('arr paramater should be an array');    
+    if (typeof limit !== 'number') throw new Error('limit paramater should be a number');    
+    if (limit > 200) throw new Error('limit paramater should be below 200');    
     if (arr.length > limit) arr.length = limit;
     return arr;
 };
 
 module.exports = {
     scrollToBottom,
+    shuffleArray,
     probability,
     humanDelay,
     randomInArray,
